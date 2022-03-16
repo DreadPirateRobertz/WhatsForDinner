524d60f97e53cb142a46be243388389ef8f27e30
amfunk <amfunk14@gmail.com>
2022-03-15 19:26:13 -0600

    added recipe search screen

--- app/src/main/java/ramseybros/WhatsForDinner/ui/screens/RecipeSearchScreen.kt
+++ app/src/main/java/ramseybros/WhatsForDinner/ui/screens/RecipeSearchScreen.kt
@@ -1,2 +1,23 @@
 package ramseybros.WhatsForDinner.ui.screens
 
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun RecipeSearchScreen() {
+    Text(
+        textAlign = TextAlign.Center,
+        text = "Recipe Search",
+        fontSize = 24.sp
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    RecipeSearchScreen()
+}
